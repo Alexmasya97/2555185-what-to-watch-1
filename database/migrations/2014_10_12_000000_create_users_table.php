2e8c7ac85891df872e32c3e94756af325de3b1d2
Alexmasya97 <sasa_97@mail.ru>
2026-03-12 22:23:42 +1000

    add models and migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+          $table->id();
+          $table->string('name');
+          $table->string('email')->unique();
+          $table->string('password');
+          $table->string('avatar')->nullable();
+          $table->enum('role', ['user', 'moderator'])->default('user');
+          $table->string('api_token', 80)->unique()->nullable()->default(null);
+          $table->timestamps();
         });
     }
 
