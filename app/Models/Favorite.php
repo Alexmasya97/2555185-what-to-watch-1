c93fe5b2a4c115bbc83d4520362a2ac783837328
Alexmasya97 <sasa_97@mail.ru>
2026-03-21 10:04:18 +1000

    add update models

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Favorite extends Model
 {
     use HasFactory;
+
+  protected $fillable = ['user_id', 'film_id'];
+
+  public function user():BelongsTo
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  public function film():BelongsTo
+  {
+    return $this->belongsTo(Film::class);
+  }
 }
